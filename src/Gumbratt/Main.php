947d80ba1b6805dd1164b5e2805d9a4850b91a57
Gumbrat <calebcrawford29@gmail.com>
2015-11-19 11:13:10 -0600

    Main.php

--- src/Gumbratt/Main.php
+++ src/Gumbratt/Main.php
@@ -1,1 +1,21 @@
-
+<?php
+	namespace Gumbratt\unlimited;
+	
+	use pocketmine\plugin\PluginBase as Plugin;
+	use pocketmine\event\Listener;
+	use pocketmine\event\player\PlayerKickEvent;
+			
+	class Loader extends Plugin implements Listener {
+		public function onEnable() {
+			$this->getServer()->getPluginManager()->registerEvents($this, $this);
+			$this->getServer()->getLogger()->info("Unlimited Enabled!");
+		}
+		
+		public function onPlayerKick(PlayerKickEvent $event) {
+			if($event->getReason() === "disconnectionScreen.serverFull")
+				$event->setCancelled(true);
+		}
+		
+		public function onDisable() {
+			$this->getServer()->getLogger()->info("Unlimited is no longer enabled! Did the server stop?");
+		}
